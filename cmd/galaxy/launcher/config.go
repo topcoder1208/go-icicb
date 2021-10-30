dc08fd4c3889c781b13711f06a918e0845a766b1
galaxy126 <galaxy85126@gmail.com>
2021-10-30 14:54:10 +0800

    fix: genesis

--- cmd/galaxy/launcher/config.go
+++ cmd/galaxy/launcher/config.go
@@ -92,7 +92,7 @@ var (
 
 	AllowedGalaxyGenesisHashes = map[uint64]hash.Hash{
 		galaxy.MainNetworkID: hash.HexToHash("0x4189c067658bf8378de9e0535e2440e7ca08be9cffe6451fbd0c6e3a1e3aee5d"),
-		galaxy.TestNetworkID: hash.HexToHash("0x2762033967a84f07f3dee05ec9783d1858883fd729a0d8cd96dc147f33e9c762"),
+		galaxy.TestNetworkID: hash.HexToHash("0x8addac1d0e03d034051f43c990a44511a9c3970ccbf0c0d50f777d2457660e4c"),
 	}
 )
 
